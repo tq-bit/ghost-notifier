fb2709aa35d2c21f22e1ec9c242721c265325234
q-bit <t.quante@outlook.com>
2023-01-23 14:53:16 +0100

    chore: remove console logs

--- src/server/util/converter.util.ts
+++ src/server/util/converter.util.ts
@@ -12,12 +12,9 @@ import crypto from 'crypto';
 export default {
 	convertIncomingWebhookToArticle: (req: Request) => {
 		const incomingPost = req.body as GhostWebhook;
-		console.log(incomingPost);
 		if (Object.keys(incomingPost.post.current).length > 0) {
-			console.log('current');
 			return incomingPost.post.current as GhostArticle;
 		}
-		console.log('previous');
 		return incomingPost.post.previous as GhostArticle;
 	},
 
