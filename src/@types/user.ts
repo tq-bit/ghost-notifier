f616f48eda86c0cb562a2f79be9c2f2fbd014263
q-bit <t.quante@outlook.com>
2023-01-30 13:17:43 +0100

    feat: Add user creation types

---
+++ src/@types/user.ts
@@ -0,0 +1,21 @@
+import { Static, Type } from '@sinclair/typebox';
+
+export enum UserRole {
+	Admin = 'admin',
+}
+
+export const UserCreationForm = Type.Object({
+	name: Type.String(),
+	email: Type.String(),
+	password: Type.String(),
+});
+
+export const User = Type.Object({
+	name: Type.String(),
+	email: Type.String(),
+	passwordHash: Type.String(),
+	role: Type.Enum(UserRole),
+});
+
+export type User = Static<typeof User>;
+export type UserCreationForm = Static<typeof UserCreationForm>;
