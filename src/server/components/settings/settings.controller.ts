76f05ea8e772d323bae782fe5955ec6bcadddb70
q-bit <t.quante@outlook.com>
2023-02-06 15:01:25 +0100

    fix: add missing response

--- src/server/components/settings/settings.controller.ts
+++ src/server/components/settings/settings.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import AppConfig from '../../config/app.config';
+import Responder from '../../util/responder.util';
+import { GN_SUCCESS_STATUS } from '../../../constants';
 
 export default {
 	toggleEnableUserCreation: async (req: Request, res: Response) => {
@@ -9,5 +11,11 @@ export default {
 			...appConfig,
 			enableUserCreation: !appConfig.enableUserCreation,
 		});
+
+		const message = `${appConfig.enableUserCreation ? 'Disabled' : 'Enabled'} user creation`;
+		return new Responder(req.headers['content-type'] || 'text/html', {
+			onJson: () => res.status(200).send({ status: GN_SUCCESS_STATUS, message: message }),
+			onOther: () => res.redirect(`/settings?status=${GN_SUCCESS_STATUS}&message=${message}`),
+		}).send();
 	},
 };
