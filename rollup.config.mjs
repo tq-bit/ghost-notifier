ec6d8d3f808c3aff707bfcfd4948e5249f21b1e3
Tq-bit <t.quante@outlook.com>
2023-05-24 08:15:37 +0200

    fix: add external packages to rollup #1

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,81 +6,86 @@ import scss from 'rollup-plugin-scss';
  */
 
 export default [
-	{
-		input: 'src/server/index.ts',
-		output: {
-			file: './dist/server.js',
-			format: 'cjs',
-		},
-		external: [
-			'crypto',
-			'cors',
-			'dotenv',
-			'express',
-			'express-handlebars',
-			'mongodb',
-			'ajv',
-			'@sinclair/typebox',
-			'winston',
-		],
-		plugins: [typescript()],
-	},
+  {
+    input: 'src/server/index.ts',
+    output: {
+      file: './dist/server.js',
+      format: 'cjs',
+    },
+    external: [
+      'cookie-parser',
+      'jsonwebtoken',
+      'fs/promises',
+      'bcrypt',
+      'path',
+      'crypto',
+      'cors',
+      'dotenv',
+      'express',
+      'express-handlebars',
+      'mongodb',
+      'ajv',
+      '@sinclair/typebox',
+      'winston',
+    ],
+    plugins: [typescript()],
+  },
 
-	// Common Javascript for all pages
-	{
-		input: 'src/client/index.ts',
-		output: {
-			file: './public/js/main.js',
-			format: 'cjs',
-		},
-		plugins: [typescript(), scss()],
-	},
+  // Common Javascript for all pages
+  {
+    input: 'src/client/index.ts',
+    output: {
+      file: './public/js/main.js',
+      format: 'cjs',
+    },
+    plugins: [typescript(), scss()],
+  },
 
-	// View components
-	{
-		input: 'src/client/views/domain.ts',
-		output: {
-			file: './public/js/domain.js',
-			format: 'cjs',
-		},
-		plugins: [typescript()],
-	},
+  // View components
+  {
+    input: 'src/client/views/domain.ts',
+    output: {
+      file: './public/js/domain.js',
+      format: 'cjs',
+    },
+    plugins: [typescript()],
+  },
 
-	{
-		input: 'src/client/views/domain-notifications.ts',
-		output: {
-			file: './public/js/domain-notifications.js',
-			format: 'cjs',
-		},
-		plugins: [typescript()],
-	},
+  {
+    input: 'src/client/views/domain-notifications.ts',
+    output: {
+      file: './public/js/domain-notifications.js',
+      format: 'cjs',
+    },
+    plugins: [typescript()],
+  },
 
-	{
-		input: 'src/client/views/login.ts',
-		output: {
-			file: './public/js/login.js',
-			format: 'cjs',
-		},
-		plugins: [typescript()],
-	},
+  {
+    input: 'src/client/views/login.ts',
+    output: {
+      file: './public/js/login.js',
+      format: 'cjs',
+    },
+    plugins: [typescript()],
+  },
 
-	{
-		input: 'src/client/views/signup.ts',
-		output: {
-			file: './public/js/signup.js',
-			format: 'cjs',
-		},
-		plugins: [typescript()],
-	},
+  {
+    input: 'src/client/views/signup.ts',
+    output: {
+      file: './public/js/signup.js',
+      format: 'cjs',
+    },
+    plugins: [typescript()],
+  },
 
-	// Styles
-	{
-		input: 'src/client/styles/styles.main.ts',
-		output: {
-			file: './public/css/main.js',
-			format: 'cjs',
-			assetFileNames: '[name][extname]',
-		},
-		plugins: [typescript(), scss()],
-	},
+  // Styles
+  {
+    input: 'src/client/styles/styles.main.ts',
+    output: {
+      file: './public/css/main.js',
+      format: 'cjs',
+      assetFileNames: '[name][extname]',
+    },
+    plugins: [typescript(), scss()],
+  },
 ];
